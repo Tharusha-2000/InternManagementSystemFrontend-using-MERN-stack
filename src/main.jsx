11d2501f5e7fc1fb5664909be86406f2909b88a3
NimnadiJayasundara <nimnadijmh.21@uom.lk>
2024-03-19 10:04:01 +0530

    Add new sidebar

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import HS from './components/common/HS.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HS />
+    <App />
   </React.StrictMode>,
 
 
