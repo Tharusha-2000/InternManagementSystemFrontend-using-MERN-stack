2739bf50d3c2264e0155819ea3ca1908545c62c9
Tharusha-2000 <112651385+Tharusha-2000@users.noreply.github.com>
2024-03-23 11:14:17 +0530

    change

--- src/components/common/admin_page/Registration.jsx
+++ src/components/common/admin_page/Registration.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import Sidebar from './Sidebar';
 import Header from '../Header';
 import Box from '@mui/material/Box';
-import Adduser from '../../adduser/Adduser';
 import Addusertable from '../../adduser/Addusertable';
-import Test from '../../../test';
 
 
 export default function Registration() {
@@ -15,7 +13,7 @@ export default function Registration() {
     <Box sx={{ display: 'flex' }}>
     <Sidebar />
     <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-      
+         <Addusertable/>
       
       </Box>
       </Box>
