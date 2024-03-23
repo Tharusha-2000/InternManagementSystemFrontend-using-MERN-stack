131a9d89d3e1361620d5b6b12f36315d2ae70f13
DilumCA <andradiwmdc.21@uom.lk>
2024-03-23 23:22:58 +0530

    evaluation intern list

---
+++ src/components/EvaluationInternList/EvaluationInternList.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+
+function EvaluationInternList() {
+  const [interns, setInterns] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/interns")
+      .then((res) => res.json())
+      .then((data) => {
+        setInterns(data);
+        setError(null);
+      })
+      .catch((err) => {
+        setError("Error fetching data");
+      });
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <TableContainer component={Paper}>
+      <Typography variant="h3" gutterBottom>
+        All Evaluations
+      </Typography>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>User ID</TableCell>
+            <TableCell>Status</TableCell>
+            <TableCell style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}>Evaluation Form</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {interns.map((intern) => (
+            <TableRow key={intern._id}>
+              <TableCell>{intern.Name}</TableCell>
+              <TableCell>{intern.userID}</TableCell>
+              <TableCell>{intern.eformStatus}</TableCell>
+              <TableCell
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <AssignmentIndIcon />
+              </TableCell>
+              <TableCell>
+                <DeleteForeverIcon></DeleteForeverIcon>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default EvaluationInternList;
