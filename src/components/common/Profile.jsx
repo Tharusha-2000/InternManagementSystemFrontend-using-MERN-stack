ba59ce930963a0ff6f36e2cb911d3ae485e3c191
Tharusha-2000 <112651385+Tharusha-2000@users.noreply.github.com>
2024-04-09 01:36:00 +0530

    edit and create user profile

--- src/components/common/Profile.jsx
+++ src/components/common/Profile.jsx
@@ -10,17 +10,14 @@ import Box from "@mui/joy/Box";
 import Button from "@mui/joy/Button";
 import Divider from "@mui/joy/Divider";
 import { FormControl, FormLabel, Select, MenuItem } from "@mui/joy";
-
 import Input from "@mui/joy/Input";
 import IconButton from "@mui/joy/IconButton";
 import Stack from "@mui/joy/Stack";
-import Option from "@mui/joy/Option";
 import Typography from "@mui/joy/Typography";
 import Card from "@mui/joy/Card";
 import CardActions from "@mui/joy/CardActions";
 import CardOverflow from "@mui/joy/CardOverflow";
 import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
-import TextField from "@mui/material/TextField";
 import EditRoundedIcon from "@mui/icons-material/EditRounded";
 import axios from "axios";
 import { BASE_URL } from "../../config";
@@ -34,8 +31,11 @@ export default function Profile() {
     role: "",
     gender: "",
     email: "",
+    jobtitle: '',
+    department: '',
+    employmentType: ''
   });
-
+  const [originalData, setOriginalData] = useState({});
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
@@ -67,11 +67,32 @@ export default function Profile() {
       })
       .then((result) => {
         setData(result.data.user);
+        setOriginalData(result.data.user);
        
         //setImageUrl(result.data.user.image);
       })
       .catch((err) => console.log(err));
   }, []);
+  
+const handleCancel = () => {
+    // Reset the form data to the original data
+    setData(originalData);
+  };
+
+const handleSubmit = (e) => {
+   e.preventDefault();
+   const token = localStorage.getItem("token");
+ axios
+    .put(`${BASE_URL}updateuser`, data, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+ };
  
   return (
     <>
@@ -79,6 +100,8 @@ export default function Profile() {
       <Box height={60} />
       <Box sx={{ display: "flex" }}>
         {getSidebar()}
+
+        <form onSubmit={handleSubmit}>
         <Box sx={{ flex: 1, width: "100%" }}>
           <Stack
             spacing={4}
@@ -144,7 +167,9 @@ export default function Profile() {
                         gap: 2,
                       }}
                     >
-                      <Input size="sm" value={data.fname} />
+                      <Input size="sm" value={data.fname} 
+                       onChange={e => setData({ ...data, fname: e.target.value })} 
+                       />
                     </FormControl>
                     <FormControl
                       sx={{
@@ -155,6 +180,7 @@ export default function Profile() {
                       <Input
                         size="sm"
                         value={data.lname}
+                        onChange={e => setData({ ...data, lname: e.target.value })} 
                         sx={{ flexGrow: 1 }}
                       />
                     </FormControl>
@@ -162,7 +188,8 @@ export default function Profile() {
                   <Stack direction="row" spacing={2}>
                     <FormControl>
                       <FormLabel>Role</FormLabel>
-                      <Input size="sm" value={data.role} />
+                      <Input size="sm" value={data.role} 
+                      onChange={e => setData({ ...data, role: e.target.value })} />
                     </FormControl>
                     <FormControl sx={{ flexGrow: 1 }}>
                       <FormLabel>Email</FormLabel>
@@ -170,6 +197,7 @@ export default function Profile() {
                         size="sm"
                         type="email"
                         value={data.email}
+                        onChange={e => setData({ ...data, email: e.target.value })}
                         startDecorator={<EmailRoundedIcon />}
                         sx={{ flexGrow: 1 }}
                       />
@@ -184,6 +212,7 @@ export default function Profile() {
                         <Input
                       size="sm"
                       value={data.gender}
+                      onChange={e => setData({ ...data, gender: e.target.value })}
                       type="text"
                       fullWidth
                   
@@ -195,6 +224,7 @@ export default function Profile() {
                           size="sm"
                           value={data.dob}
                           type="date"
+                          onChange={e => setData({ ...data, dob: e.target.value })}
                           fullWidth
                        
                         />
@@ -251,7 +281,8 @@ export default function Profile() {
                         gap: 2,
                       }}
                     >
-                      <Input size="sm" value={data.fname} />
+                      <Input size="sm" value={data.fname}
+                      onChange={e => setData({ ...data, fname: e.target.value })} />
                     </FormControl>
                     <FormControl
                       sx={{
@@ -262,13 +293,15 @@ export default function Profile() {
                         gap: 2,
                       }}
                     >
-                      <Input size="sm" value={data.lname} />
+                      <Input size="sm" value={data.lname} 
+                      onChange={e => setData({ ...data, lname: e.target.value })}/>
                     </FormControl>
                   </Stack>
                 </Stack>
                 <FormControl>
                   <FormLabel>Role</FormLabel>
-                  <Input size="sm" value={data.role} />
+                  <Input size="sm" value={data.role}
+                  onChange={e => setData({ ...data, role: e.target.value })} />
                 </FormControl>
                 <FormControl sx={{ flexGrow: 1 }}>
                   <FormLabel>Email</FormLabel>
@@ -277,6 +310,7 @@ export default function Profile() {
                     type="email"
                     startDecorator={<EmailRoundedIcon />}
                     value={data.email}
+                    onChange={e => setData({ ...data, email: e.target.value })}
                     sx={{ flexGrow: 1 }}
                   />
                 </FormControl>
@@ -288,6 +322,7 @@ export default function Profile() {
                       size="sm"
                       value={data.gender}
                       type="text"
+                      onChange={e => setData({ ...data, gender: e.target.value })}
                       fullWidth
                   
                     />
@@ -300,27 +335,76 @@ export default function Profile() {
                       size="sm"
                       value={data.dob}
                       type="date"
+                      onChange={e => setData({ ...data, dob: e.target.value })}
                       fullWidth
                   
                     />
                   </FormControl>
                 </div>
               </Stack>
-              <CardOverflow
-                sx={{ borderTop: "1px solid", borderColor: "divider" }}
-              >
-                <CardActions sx={{ alignSelf: "flex-end", pt: 2 }}>
-                  <Button size="sm" variant="outlined" color="neutral">
-                    Cancel
-                  </Button>
-                  <Button size="sm" variant="solid">
-                    Save
-                  </Button>
-                </CardActions>
-              </CardOverflow>
+             
             </Card>
+          
+        <Card>
+          <Box sx={{ mb: 1 }}>
+            <Typography level="title-md">job details</Typography>
+          </Box>
+          <Divider />
+          <div>
+                    <Stack direction="row" spacing={2}>
+                      <FormControl sx={{ flexGrow: 1 }}>
+                     
+                        <FormLabel>Job title</FormLabel>
+                        <Input
+                      size="sm"
+                      value={data.jobtitle}
+                      type="text"
+                      onChange={e => setData({ ...data, jobtitle: e.target.value })}
+                      fullWidth
+                  
+                    />
+                      </FormControl>
+                      <FormControl sx={{ flexGrow: 1 }}>
+                        <FormLabel>Department</FormLabel>
+                        <Input
+                          size="sm"
+                          value={data.department}
+                          onChange={e => setData({ ...data, department: e.target.value })}
+                          type="text"
+                          fullWidth
+                       
+                        />
+                      </FormControl>
+                    </Stack>
+                  </div>
+
+           <FormControl sx={{ display: { sm: "contents" } }}>
+                    <FormLabel>employmentType</FormLabel>
+                    <Input
+                      size="sm"
+                      value={data.employmentType}
+                      type="text"
+                      onChange={e => setData({ ...data, employmentType: e.target.value })}
+                      fullWidth
+                  
+                    />
+           </FormControl>
+
+          <CardOverflow sx={{ borderTop: '1px solid', borderColor: 'divider' }}>
+            <CardActions sx={{ alignSelf: 'flex-end', pt: 2 }}>
+              <Button size="sm" variant="outlined" color="neutral" onClick={handleCancel}>
+                Cancel
+              </Button>
+              <Button size="sm" variant="solid" type="submit">
+                Save
+              </Button>
+            </CardActions>
+          </CardOverflow>
+        </Card>
           </Stack>
+            
         </Box>
+        </form>
       </Box>
     </>
   );
