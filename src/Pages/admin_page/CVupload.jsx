5a598161acd317c9714945a66de83ed7113f87de
NimnadiJayasundara <nimnadijmh.21@uom.lk>
2024-04-17 17:42:30 +0530

    create cvuploadfiles

--- src/Pages/admin_page/CVupload.jsx
+++ src/Pages/admin_page/CVupload.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import AdminSidebar from '../../components/common/AdminSidebar';
 import Header from '../../components/common/Header';
+import '../../index.css';
 import Box from '@mui/material/Box';
+import InternCvList from './CVuploadFiles/InternCvList';
 
 
 export default function CVupload() {
   return (
     <>
+    <div className="bgcolor">
     <Header />
+    <Box height={80} />
     <Box sx={{ display: 'flex' }}>
     <AdminSidebar />
     <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+      <InternCvList />
       </Box>
       </Box>
+      </div>
       </>
   )}
