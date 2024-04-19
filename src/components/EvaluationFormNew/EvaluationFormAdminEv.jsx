25ab3a701778ee8aefb9c5e02ae3281f9003daa3
DilumCA <andradiwmdc.21@uom.lk>
2024-04-19 21:28:37 +0530

    evaluation form admin complete code

---
+++ src/components/EvaluationFormNew/EvaluationFormAdminEv.jsx
@@ -0,0 +1,333 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, Typography, Box, InputLabel, Select, MenuItem, TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, Radio, TextField, Button, FormControl } from '@mui/material';
+import CheckIcon from '@mui/icons-material/Check';
+
+function EvaluationFormAdminEv({ evaluationFormDetailsId,handleSave, additionalCriteria, setAdditionalCriteria, additionalCriteria2, setAdditionalCriteria2,initialCriteria, initialCriteria2,  setSelectedEvaluator,selectedEvaluator, evaluatorError, setEvaluatorError  }) {
+    
+    
+   
+    
+    
+    const [ratings, setRatings] = useState([]);
+    const [ratings2, setRatings2] = useState([]);
+    const [newCriteria, setNewCriteria] = useState('');
+    const [newCriteria2, setNewCriteria2] = useState('');
+    const [evaluators, setEvaluators] = useState([]);//for select evaluators
+
+
+    useEffect(() => {
+        console.log("Evaluation Form Details ID in EvaluationFormAdminEv component:", evaluationFormDetailsId);
+      
+        axios.get('http://localhost:8000/api/users/evaluators')
+          .then(response => {
+            setEvaluators(response.data);
+          })
+          .catch(error => {
+            console.error('There was an error!', error);
+          });
+      
+        console.log(selectedEvaluator);
+      }, [evaluationFormDetailsId, selectedEvaluator]); // Both dependencies in a single array
+    
+     
+      const handleChange = (event) => {
+        setSelectedEvaluator(event.target.value);
+        setEvaluatorError(false); // Reset evaluatorError when an evaluator is selected
+        console.log(event.target.value); // Log the selected value
+      };
+  
+    const handleRadioChange = (event, index) => {
+      setRatings((prevRatings) => {
+        const newRatings = [...prevRatings];
+        newRatings[index] = Number(event.target.value);
+        return newRatings;
+      });
+    };
+    
+    const handleAddCriteria = () => {
+      setAdditionalCriteria((prevValues) => [...prevValues, newCriteria]);
+      setNewCriteria("");
+    };
+  
+    const handleRemoveCriteria = (index) => {
+      setAdditionalCriteria((prevValues) => prevValues.filter((_, i) => i !== index));
+    };
+    
+  return (
+    <div>
+      <Typography variant="h4" align="center" style={{ margin: "20px 0" }}>
+        Evaluator
+      </Typography>
+
+      <Container maxWidth="md">
+      <br></br>
+
+      <FormControl fullWidth>
+                    <InputLabel id="evaluator-select-label">Select Evaluator</InputLabel>
+                    <Select
+                        labelId="evaluator-select-label"
+                        id="evaluator-select"
+                        onChange={handleChange}
+                        value={selectedEvaluator}
+                        label="Select Evaluator"
+                        error={evaluatorError} // error prop
+                    >
+                        {evaluators.map((evaluator, index) => (
+  <MenuItem key={index} value={evaluator}>{evaluator}</MenuItem>
+))}
+                    </Select>
+                    {evaluatorError && <p style={{ color: 'red' }}>Please select an evaluator.</p>} 
+       
+                </FormControl>
+               
+                
+    </Container>
+
+      <br></br>
+      <Container maxWidth="md">
+        <Typography variant="h5" align="left" style={{ margin: "20px 0", fontWeight: "bold" }}>
+          Rating Scale
+        </Typography>
+        <Box display="flex" justifyContent="space-between" width="100%">
+          <Typography variant="body1">5 – outstanding</Typography>
+          <Typography variant="body1">4 – exceeds expectations</Typography>
+          <Typography variant="body1">3 – meets expectations</Typography>
+          <Typography variant="body1">2 – needs improvement</Typography>
+          <Typography variant="body1">1 – unacceptable</Typography>
+        </Box>
+
+        <br></br>
+      </Container>
+      <br></br>
+
+      <Container maxWidth="md">
+
+      <Typography variant="h5" align="left" style={{ margin: "20px 0", fontWeight: "bold" }}>
+        Criterias for Assessing Job Performance
+        </Typography>
+        <br></br>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell align="left" colSpan={1}>
+                  <Typography variant="h6">criteria</Typography>
+                </TableCell>
+                {['a', 'b', 'c', 'd', 'e'].map((value, num) => (
+                  <TableCell key={num} align="center">
+                    <Typography variant="h6">{num + 1}</Typography>
+                  </TableCell>
+                ))}
+                <TableCell align="center">
+                  <Typography variant="h6">Weight</Typography>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {[...initialCriteria, ...additionalCriteria].map((criteria, index) => (
+                <TableRow key={index}>
+                  <TableCell component="th" scope="row">
+                    {criteria}
+                  </TableCell>
+                  {['a', 'b', 'c', 'd', 'e'].map((value, num) => (
+                    <TableCell key={num} align="center">
+                      <Radio
+                        value={num + 1}
+                        checked={ratings[index] === num + 1}
+                        onChange={(event) => handleRadioChange(event, index)}
+                        disabled
+                      />
+                    </TableCell>
+                  ))}
+                  <TableCell align="center">
+                    <TextField
+                      value={ratings[index] ? ratings[index] * 20 + '%' : ''}
+                      disabled
+                      // Add your other TextField properties here
+                    />
+                  </TableCell>
+                  {index >= initialCriteria.length && (
+                    <TableCell align="center">
+                      <Button onClick={() => handleRemoveCriteria(index - initialCriteria.length)}>Remove</Button>
+                    </TableCell>
+                  )}
+                </TableRow>
+              ))}
+            <TableRow>
+  <TableCell>
+    <TextField
+      value={newCriteria}
+      onChange={(e) => setNewCriteria(e.target.value)}
+    />
+  </TableCell>
+  <TableCell />
+  <TableCell />
+  <TableCell />
+  <TableCell />
+  <TableCell />
+  <TableCell align="right">
+  <Button onClick={handleAddCriteria} startIcon={<CheckIcon sx={{ fontSize: 40 }} />}>ADD</Button>
+</TableCell>
+</TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
+
+<Container maxWidth="md">
+  <Typography variant="h5" align="left" style={{ margin: "20px 0", fontWeight: "bold" }}>
+  CORE VALUES AND OBJECTIVES
+  </Typography>
+  <br></br>
+  <TableContainer component={Paper}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell align="left" colSpan={1}>
+            <Typography variant="h6">criteria</Typography>
+          </TableCell>
+          {['a', 'b', 'c', 'd', 'e'].map((value, num) => (
+            <TableCell key={num} align="center">
+              <Typography variant="h6">{num + 1}</Typography>
+            </TableCell>
+          ))}
+          <TableCell align="center">
+            <Typography variant="h6">Weight</Typography>
+          </TableCell>
+        </TableRow>
+      </TableHead>
+
+      <TableBody>
+  {[...initialCriteria2, ...additionalCriteria2].map((criteria, index) => (
+    <TableRow key={index}>
+      <TableCell component="th" scope="row">
+        {criteria}
+      </TableCell>
+      {['a', 'b', 'c', 'd', 'e'].map((value, num) => (
+        <TableCell key={num} align="center">
+          <Radio
+            value={num + 1}
+            checked={ratings2[index] === num + 1}
+            onChange={(event) => setRatings2((prevRatings) => ({ ...prevRatings, [index]: Number(event.target.value) }))}
+            disabled
+          />
+        </TableCell>
+      ))}
+      <TableCell align="center">
+        <TextField
+          value={ratings2[index] ? ratings2[index] * 20 + '%' : ''}
+          disabled
+          // Add your other TextField properties here
+        />
+      </TableCell>
+      {index >= initialCriteria2.length && (
+        <TableCell align="center">
+          <Button onClick={() => setAdditionalCriteria2((prevValues) => prevValues.filter((_, i) => i !== index - initialCriteria2.length))}>Remove</Button>
+        </TableCell>
+      )}
+    </TableRow>
+  ))}
+  <TableRow>
+    <TableCell>
+      <TextField
+        value={newCriteria2}
+        onChange={(e) => setNewCriteria2(e.target.value)}
+      />
+    </TableCell>
+    <TableCell />
+    <TableCell />
+    <TableCell />
+    <TableCell />
+    <TableCell />
+    <TableCell align="right">
+
+      <Button onClick={() => { setAdditionalCriteria2((prevValues) => [...prevValues, newCriteria2]); setNewCriteria2(""); }} startIcon={<CheckIcon sx={{ fontSize: 40 }} />}>ADD</Button>
+    </TableCell>
+  </TableRow>
+
+</TableBody>
+    </Table>
+  </TableContainer>
+  
+  
+  <br></br>
+  <Box display="flex" alignItems="center" justifyContent="space-between">
+  <Typography variant="h6" sx={{ paddingLeft: '20px' }}>Total points</Typography>
+  <TextField />
+</Box>
+
+
+  
+</Container>
+
+<Container maxWidth="md">
+  <Typography variant="h5" align="left" style={{ margin: "20px 0", fontWeight: "bold" }}>
+    Overall Performance
+  </Typography>
+  <br></br>
+  <TableContainer component={Paper}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell align="left" colSpan={1}>
+            <Typography variant="h6">criteria</Typography>
+          </TableCell>
+          {['a', 'b', 'c', 'd', 'e'].map((value, num) => (
+            <TableCell key={num} align="center">
+              <Typography variant="h6">{num + 1}</Typography>
+            </TableCell>
+          ))}
+          <TableCell align="center">
+            <Typography variant="h6">Weight</Typography>
+          </TableCell>
+        </TableRow>
+      </TableHead>
+
+      <TableBody>
+        <TableRow>
+          <TableCell component="th" scope="row">
+            Overall Performance
+          </TableCell>
+          {['a', 'b', 'c', 'd', 'e'].map((value, num) => (
+            <TableCell key={num} align="center">
+              <Radio
+                value={num + 1}
+                disabled
+                
+                // Add your radio button properties here
+              />
+            </TableCell>
+          ))}
+          <TableCell align="center">
+            <TextField
+            disabled
+              // Add your TextField properties here
+            />
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </TableContainer>
+</Container>
+
+<br></br><br></br>
+<Container  maxWidth="md">
+<Box display="flex" flexDirection="column" alignItems="flex-start" justifyContent="space-between">
+  <Typography variant="h6" align="left" sx={{ paddingLeft: '20px' }}>comments /Reccomendations</Typography>
+</Box>
+</Container>
+<Container  maxWidth="md">
+    <br></br>
+<Box display="flex" flexDirection="column" alignItems="flex-end" justifyContent="space-between">
+  <TextField multiline rows={4} sx={{ width: '700px' }} disabled />
+</Box>
+</Container>
+
+<br></br>
+    </div>
+  )
+}
+
+export default EvaluationFormAdminEv;
--- src/components/EvaluationInternList/EvaluationInternList.jsx
+++ src/components/EvaluationInternList/EvaluationInternList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import {
   Table,
   TableBody,
@@ -10,72 +10,222 @@ import {
   Paper,
   IconButton,
   Typography,
+  Dialog,
+  DialogContent,
+  DialogActions,
+  Button,
 } from "@mui/material";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import EvaluationFormAdminFu from "../EvaluationFormNew/EvaluationFormAdminFu";
 
 function EvaluationInternList() {
   const [interns, setInterns] = useState([]);
   const [error, setError] = useState(null);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [selectedInternName, setSelectedInternName] = useState("");
+  const [selectedMentorName, setSelectedMentorName] = useState("");
+  const [selectedEvaluationFormDetailsId, setSelectedEvaluationFormDetailsId] =
+    useState("");
+  const [saveDialogOpen, setSaveDialogOpen] = useState(false); // Add this line
+  const [completedDialogOpen, setCompletedDialogOpen] = useState(false); // to implement dialog box for notify completed evaluation forms
+  const [refreshKey, setRefreshKey] = useState(0); // to refresh the page after save the evaluation form
+  const [confirmDialogOpen, setConfirmDialogOpen] = useState(false);
+  const [toBeDeletedId, setToBeDeletedId] = useState(null);
+
+  const handleClick = (intern) => {
+    if (intern.eformStatus === "created") {
+      setCompletedDialogOpen(true); // Open the "already created" dialog
+    } else {
+      setSelectedInternName(intern.name);
+      setSelectedMentorName(intern.mentor);
+      setSelectedEvaluationFormDetailsId(intern.evaluationFormDetailsId);
+      setOpenDialog(true); // Open the normal dialog
+    }
+  };
+  const handleClose = () => {
+    setOpenDialog(false);
+  };
 
   useEffect(() => {
     axios
-      .get("http://localhost:8001/api/users/interns")
+      .get("http://localhost:8000/api/users/Evinterns")
       .then((result) => {
+        console.log(result.data); // Log the data to see what's returned
         setInterns(result.data);
       })
       .catch((err) => {
         console.log(err);
         setError(err.message);
       });
-  }, []);
+  }, [refreshKey]);
 
   if (error) {
     return <div>Error: {error}</div>;
   }
+
+  const handleDelete = (id) => {
+    axios
+      .delete("http://localhost:8000/api/users/deleteeformData", {
+        data: { id },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setRefreshKey((oldKey) => oldKey + 1); // Refresh the component
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   return (
     <TableContainer component={Paper}>
       <Typography variant="h3" gutterBottom>
         All Evaluations
       </Typography>
-      <Table aria-label="simple table">
-        <TableHead>
+      <Table
+        aria-label="simple table"
+        sx={{ minWidth: 650, backgroundColor: "#f5f5f5" }}
+      >
+        <TableHead sx={{ backgroundColor: "#e0e0e0" }}>
           <TableRow>
-            <TableCell>Intern Name</TableCell>
-            <TableCell>Mentor Name</TableCell>
-            <TableCell>Status</TableCell>
-            <TableCell style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}>Evaluation Form</TableCell>
+            <TableCell sx={{ fontWeight: "bold", fontSize: "1.2rem" }}>
+              Intern Name
+            </TableCell>
+            <TableCell sx={{ fontWeight: "bold", fontSize: "1.2rem" }}>
+              Mentor Name
+            </TableCell>
+            <TableCell sx={{ fontWeight: "bold", fontSize: "1.2rem" }}>
+              Status
+            </TableCell>
+            <TableCell
+              sx={{
+                fontWeight: "bold",
+                fontSize: "1.2rem",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              Evaluation Form
+            </TableCell>
+            <TableCell sx={{ fontWeight: "bold", fontSize: "1.2rem" }}>
+              
+              </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {interns.map((intern, index) => (
-            <TableRow key={index}>
-              <TableCell>{intern.name}</TableCell>
-              <TableCell>{intern.mentor}</TableCell>
-              <TableCell>{intern.eformStatus}</TableCell>
+            <TableRow
+              key={index}
+              sx={{ "&:nth-of-type(odd)": { backgroundColor: "#fafafa" } }}
+            >
+              <TableCell sx={{ fontSize: "1rem" }}>{intern.name}</TableCell>
+              <TableCell sx={{ fontSize: "1rem" }}>{intern.mentor}</TableCell>
+              <TableCell sx={{ fontSize: "1rem" }}>
+                {intern.eformStatus}
+              </TableCell>
               <TableCell
-                style={{
+                sx={{
+                  fontSize: "1rem",
                   display: "flex",
                   justifyContent: "center",
                   alignItems: "center",
                 }}
               >
-                <AssignmentIndIcon />
+                <IconButton onClick={() => handleClick(intern)}>
+                  <AssignmentIndIcon />
+                </IconButton>
               </TableCell>
-              <TableCell>
-                <DeleteForeverIcon></DeleteForeverIcon>
+              <TableCell sx={{ fontSize: "1rem" }}>
+                <Button
+                  onClick={() => {
+                    setToBeDeletedId(intern.evaluationFormDetailsId);
+                    setConfirmDialogOpen(true);
+                  }}
+                >
+                  <DeleteForeverIcon />
+                </Button>
               </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
+
+      <Dialog open={openDialog} onClose={handleClose} maxWidth="md" fullWidth>
+        <EvaluationFormAdminFu
+          internName={selectedInternName}
+          mentorName={selectedMentorName} // Pass selected mentor name as prop
+          evaluationFormDetailsId={selectedEvaluationFormDetailsId} // Pass selected evaluationFormDetailsId as prop
+          onClose={handleClose}
+          onSave={() => {
+            handleClose(); // Close the first dialog
+            setSaveDialogOpen(true); // Open the second dialog
+            setRefreshKey((oldKey) => oldKey + 1); // Refresh the component
+          }}
+        />
+      </Dialog>
+
+      {/*Add this dialog box for notify succesfully saved pop up*/}
+      <Dialog open={saveDialogOpen} onClose={() => setSaveDialogOpen(false)}>
+        <DialogContent style={{ padding: "20px" }}>
+          <Typography variant="h5" align="center" gutterBottom>
+            Successfully saved
+          </Typography>
+          <Typography variant="subtitle1" align="center">
+            notified respected evaluators and mentors
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => setSaveDialogOpen(false)} // Close the dialog when clicked
+          >
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      {/*Add this dialog box for notify completed evaluation forms*/}
+      <Dialog
+        open={completedDialogOpen}
+        onClose={() => setCompletedDialogOpen(false)}
+      >
+        <DialogContent>
+          <Typography variant="h6" align="center">
+            Already created the evaluation form
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setCompletedDialogOpen(false)}>OK</Button>
+        </DialogActions>
+      </Dialog>
+
+      {/*Added this confirmation dialog box for confirm delete*/}
+      <Dialog
+        open={confirmDialogOpen}
+        onClose={() => setConfirmDialogOpen(false)}
+      >
+        <DialogContent>
+          <Typography variant="h6" align="center">
+            Are you sure you want to delete this intern?
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setConfirmDialogOpen(false)}>Cancel</Button>
+          <Button
+            onClick={() => {
+              handleDelete(toBeDeletedId);
+              setConfirmDialogOpen(false);
+            }}
+          >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
     </TableContainer>
   );
 }
