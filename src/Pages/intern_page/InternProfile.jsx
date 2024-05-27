4356e7d380b4bc16d69c3c7829caa33afc44ff57
Sanugi <divigalpitiyawdwmsg.21@uom.lk>
2024-05-27 15:28:17 +0530

    ...

--- src/Pages/intern_page/InternProfile.jsx
+++ src/Pages/intern_page/InternProfile.jsx
@@ -49,7 +49,7 @@ export default function Profile() {
         //setImageUrl(result.data.user.image);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, []);  //If there is an error during the GET request, this block of code is executed. It logs the error to the console.
 
  const handleCancel = () => {
     // Reset the form data to the original data
@@ -63,7 +63,7 @@ const handleSubmit = (e) => {
       headers: { Authorization: `Bearer ${token}` },
     })
     .then((response) => {
-      window.alert(response.data.msg);
+      window.alert(response.data.msg); // display the response data in an alert box
       console.log(response.data);
     })
     .catch((error) => {
@@ -139,10 +139,12 @@ return (
                   <Input size="sm" 
                       value={data.fname}
                       onChange={e => setData({ ...data, fname: e.target.value })}
+                      placeholder="First name"
                       />
                   <Input size="sm" sx={{ flexGrow: 1 }} 
                     value={data.lname}
                     onChange={e => setData({ ...data, lname: e.target.value })}
+                    placeholder="Last name"
                    />
                 </FormControl>
             </Stack>
@@ -175,11 +177,14 @@ return (
                   <Input size="sm" 
                    value={data.phonenumber}
                    onChange={e => setData({ ...data, phonenumber: e.target.value })} 
+                   placeholder="Phone Number"
                    />
                 </FormControl>
                 <FormControl>
                             <FormLabel>Role</FormLabel>
-                        <Input size="sm" value={data.role} />
+                        <Input size="sm" value={data.role}
+                        placeholder="Role"
+                        />
                     </FormControl>
                 </Stack>
                 </Stack>
